1f44a531833637f784ba4c065e431560b00c5505
ragavpn <106122095@nitt.edu>
2024-01-18 21:00:18 +0530

    Removed Text File Necessity

--- server/app.py
+++ server/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request
 from flask_cors import CORS
-import subprocess
 from flask_socketio import SocketIO
+from server.runner import MLModel
 
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://localhost:5000"]}})
-
+mlModel = MLModel()
 user_replies = []
 bot_replies = []
 
@@ -15,31 +15,11 @@ bot_replies = []
 def handle_connect():
     print('A client has connected to the server.')
 
+
 @app.route("/message")
 def handle_message():
     message = request.args.get('userInput')
-
-    print("Received message...")
-    userQuery = message
-    print(userQuery)
-    user_replies.append(userQuery)
-
-    file_path = "text.txt"
-
-    with open(file_path, "w") as file:
-        file.writelines([userQuery])
-
-
-    ml_file = "runner.py"    #correct it later
-    subprocess.run(['python3', ml_file])
-
-    row=""
-    with open(file_path, "r") as file:
-        row = file.read()
-    bot_replies.append(row)
-    print("Reply:",row)
-
-    return row
+    return mlModel.run(message)
 
 @app.route("/feedback")
 def handle_feedback():
--- server/runner.py
+++ server/runner.py
@@ -8,59 +8,59 @@ warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-device = torch.device("cpu")
+class MLModel:
+    def __init__(self):
+        self.device = torch.device("cpu")
 
-# Classifier pipeline for text classification (DistilBERT)
-classifier = pipeline("text-classification", model="bdotloh/distilbert-base-uncased-empathetic-dialogues-context", return_all_scores=True)
+        # Classifier pipeline for text classification (DistilBERT)
+        self.classifier = pipeline("text-classification", model="bdotloh/distilbert-base-uncased-empathetic-dialogues-context", return_all_scores=True)
 
-# Load the pre-trained RoBERTa model and tokenizer
-model_name = "gpt2"
-# saved_model_dir = "gpt2_empathy_model"
-checkpoint_path = "gpt2_empathy_checkpoint.pt"
+        # Load the pre-trained RoBERTa model and tokenizer
+        model_name = "gpt2"
+        # saved_model_dir = "gpt2_empathy_model"
+        checkpoint_path = "gpt2_empathy_checkpoint.pt"
 
-if os.path.exists(checkpoint_path):
-    checkpoint = torch.load(checkpoint_path)
-    model = GPT2LMHeadModel.from_pretrained(model_name).to(device)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    print("Loaded checkpoint")
-else:
-    print("Loading model...")
-    model = GPT2LMHeadModel.from_pretrained(model_name).to(device)
+        if os.path.exists(checkpoint_path):
+            checkpoint = torch.load(checkpoint_path)
+            self.model = GPT2LMHeadModel.from_pretrained(model_name).to(self.device)
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+            print("Loaded checkpoint")
+        else:
+            print("Loading model...")
+            self.model = GPT2LMHeadModel.from_pretrained(model_name).to(self.device)
 
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-# Set the pad_token_id to indicate the end of generated text
-model.config.pad_token_id = model.config.eos_token_id
+        # Set the pad_token_id to indicate the end of generated text
+        self.model.config.pad_token_id = self.model.config.eos_token_id
 
+    def run(self,text):
+        prediction = self.classifier(text)
+        prediction = prediction[0]
 
-text_path = 'text.txt'
+        # Sort the predictions by score in descending order
+        sorted_predictions = sorted(prediction, key=lambda x: x['score'], reverse=True)
 
-if os.path.exists(text_path):
-    with open(text_path, 'r') as file:
-        text = file.read()
+        # Find the label with the second-highest score
+        dialogue_emotion = sorted_predictions[1]['label']
+        emotion = sorted_predictions[0]['label']
 
+        prompt = f"User Input: {text}\nThis shows the emotion {emotion} and from past conversation, it also shows the emotion {dialogue_emotion} currently."
 
-prediction = classifier(text)
-prediction = prediction[0]
+        # Generate text based on the prompt using RoBERTa
+        input_ids = self.tokenizer.encode(prompt, return_tensors="pt").to(self.device)
+        output = self.model.generate(input_ids, max_length=150, num_return_sequences=1, no_repeat_ngram_size=2)
 
-# Sort the predictions by score in descending order
-sorted_predictions = sorted(prediction, key=lambda x: x['score'], reverse=True)
+        # Decode and print the generated text
+        generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        generated_text = generated_text[generated_text.find("The response to the above statement can be - ")+ len("The response to the above statement can be - ")+1:]
+        generated_text = generated_text[:generated_text.find("!!")-2]
 
-# Find the label with the second-highest score
-dialogue_emotion = sorted_predictions[1]['label']
-emotion = sorted_predictions[0]['label']
-prompt = f"User Input: {text}\nThis shows the emotion {emotion} and from past conversation, it also shows the emotion {dialogue_emotion} currently."
+        return generated_text
 
-# Generate text based on the prompt using RoBERTa
-input_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)
-output = model.generate(input_ids, max_length=150, num_return_sequences=1, no_repeat_ngram_size=2)
+# while True:
+        
+#     text = input("Enter: ")
+#     run(text)
+    
 
-# Decode and print the generated text
-generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-generated_text = generated_text.find("The response to the above statement can be - ")
-generated_text = generated_text[generated_text + len("The response to the above statement can be - ")+1:]
-generated_text = generated_text[:generated_text.find("!!")-2]
-
-if os.path.exists(text_path):
-    with open(text_path, 'w') as file:
-        file.write(generated_text)
